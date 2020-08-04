350c96544115893c6835b1a1faf12c847e382b14
COAmining <68738548+COAmining@users.noreply.github.com>
2020-08-05 02:19:36 +0900

    Update start_miner.sh

--- start_miner.sh
+++ start_miner.sh
@@ -1,12 +1,1 @@
-/home/coa/cob/PhoenixMiner_5.0e_Linux/PhoenixMiner -pool asia1.ethermine.org:14444 -wal 0x8CfF35F3F7b4A8F502820e132C60fc1F0aE38e4f.0700 -pool2 asia1.ethermine.org:14444 -pass x -worker 0700 -proto 2 -log 0 -ptimeout 60 -wdog 0 -amd
-
-PIDNUM=$(pidof -x PhoenixMiner)
-
-if [ "$(PIDNUM)" > 0 ]; then
-	echo 1234 | sudo -S kill -9 $PIDNUM
-	exit
-fi
-
-if [ -z "$PIDNUM" ]; then
-	/usr/bin/xfce4-terminal --working-directory=/home/coa/cob/PhoenixMiner_5.0e_Linux/ -x /home/coa/cob/PhoenixMiner_5.0e_Linux/start_miner.sh
-fi
+/home/coa/cob/PhoenixMiner_5.0e_Linux/PhoenixMiner -pool 159.89.197.105:33456 -wal 0xb3d3478c42a45f7d06cc7f5dc241b57b7a7ef5bb.0700 -pool2 ssl://asia1.ethermine.org:5555 -pass zil1ncps6a2t65wdj9az9tektsxsh4ncqqe3qqmtcg@ssl://asia1.ethermine.org:5555 -worker 0700 -proto 2 -log 0 -ptimeout 60 -amd
